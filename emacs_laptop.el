1682e238ce705867627bb5f29de085728acb6feb
Emanuel Heitlinger <emanuelheitlinger@gmail.com>
2010-04-06 12:58:06 +0200

    Mail norifications on laptop

--- emacs_laptop.el
+++ emacs_laptop.el
@@ -47,3 +47,27 @@
 
 (global-set-key  "\C-cmm" 'emms)
 
+;; norify send to warn me about e-mail etc...
+
+(defun djcb-popup (title msg &optional icon sound)
+  "Show a popup if we're on X, or echo it otherwise; TITLE is the title
+of the message, MSG is the context. Optionally, you can provide an ICON and
+a sound to be played"
+  (interactive)
+  (when sound (shell-command
+               (concat "mplayer -really-quiet " sound " 2> /dev/null")))
+  (if (eq window-system 'x)
+      (shell-command (concat "notify-send "
+                             (if icon (concat "-i " icon) "")
+                             " '" title "' '" msg "'"))
+    ;; text only version
+    (message (concat title ": " msg))))
+
+(add-hook 'wl-mail-send-pre-hook 'djcb-wl-draft-subject-check)
+(add-hook 'wl-mail-send-pre-hook 'djcb-wl-draft-attachment-check)
+
+(add-hook 'wl-biff-notify-hook
+          (lambda()
+            (djcb-popup "Wanderlust" "You have new mail!")))
+
+
