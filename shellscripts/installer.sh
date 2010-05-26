dc71e92855f5afe66bb0aeafa3e7414071f6cfc3
Emanuel Heitlinger <ele@ele-laptop.(none)>
2010-05-26 15:12:39 +0200

    shell-scripts to path and corrected package for emacs-w3m

--- shellscripts/installer.sh
+++ shellscripts/installer.sh
@@ -1,7 +1,9 @@
 !#/bin/bash
-yum install git xtern ratpoison most R emacs emacs-ess emacs-w3m emacs-bbdb unison keychain
+yum install git xtern ratpoison most R emacs emacs-ess w3m-el emacs-bbdb unison keychain screen
 
 ln -s dotfiles/dotemacs/main.el .emacs
 ln -s dotfiles/conkeror_rc.js .conkerorrc
 rm .bashrc
 ln -s dotfiles/bashrc.sh .bashrc
+
+ln -s /home/ele/dotfiles/shellscripts/* /usr/local/bin/
