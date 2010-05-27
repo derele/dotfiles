e4c14678b34301173d4b68bb5400bee53dd52df1
Emanuel Heitlinger <emanuelheitlinger@gmail.com>
2010-05-27 13:58:19 +0200

    oofice

--- shellscripts/installer.sh
+++ shellscripts/installer.sh
@@ -1,5 +1,5 @@
 !#/bin/bash
-yum install git xtern ratpoison most R emacs emacs-ess w3m-el emacs-bbdb unison keychain screen
+yum install git xtern ratpoison most R emacs emacs-ess w3m-el emacs-bbdb emacs-auctex unison keychain screen gnutls-utils rubber xpdf offlineimap openoffice.org-base
 
 ln -s dotfiles/dotemacs/main.el .emacs
 ln -s dotfiles/conkeror_rc.js .conkerorrc
