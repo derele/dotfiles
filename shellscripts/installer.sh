43fbe61f8d377feab14ea09c368ecb2d8d0752cb
derele <emanuelheitlinger@gmail.com>
2010-06-02 16:09:42 +0200

    yum

--- shellscripts/installer.sh
+++ shellscripts/installer.sh
@@ -1,5 +1,5 @@
 !#/bin/bash
-yum install git xtern ratpoison most R emacs emacs-ess w3m-el emacs-bbdb emacs-auctex unison keychain screen gnutls-utils rubber xpdf offlineimap openoffice.org-base sbcl aspell-de autoconf
+yum install git xtern ratpoison most R emacs emacs-ess w3m-el emacs-bbdb emacs-auctex unison keychain screen gnutls-utils rubber xpdf offlineimap openoffice.org-base sbcl aspell-de autoconf texinfo
 
 ln -s dotfiles/dotemacs/main.el .emacs
 ln -s dotfiles/conkeror_rc.js .conkerorrc
