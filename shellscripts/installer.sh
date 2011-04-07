778b53dce2212ba8befca5c5525be0d8a149e660
Emanuel <ele@hoernchen.desk>
2011-04-07 17:06:41 +0200

    learned from hoernchen.desk install

--- shellscripts/installer.sh
+++ shellscripts/installer.sh
@@ -1,9 +1,15 @@
 !#/bin/bash
-yum install git xtern ratpoison most R emacs emacs-ess w3m-el emacs-bbdb emacs-auctex unison keychain screen gnutls-utils rubber xpdf offlineimap openoffice.org-base sbcl aspell-de autoconf texinfo
+yum install git xterm ratpoison most R emacs emacs-ess w3m-el emacs-bbdb emacs-auctex unison keychain screen gnutls-utils rubber xpdf offlineimap openoffice.org-base sbcl aspell-de autoconf texinfo xulrunner
 
 ln -s dotfiles/dotemacs/main.el .emacs
 ln -s dotfiles/conkeror_rc.js .conkerorrc
+ln -s dotfiles/ratpoisonrc .ratpoisonrc
+ln -s dotfiles/Xresources .Xresources
+ln -s dotfiles/Rprofile.R .Rprofile
+
+
 rm .bashrc
 ln -s dotfiles/bashrc.sh .bashrc
+
 
 ln -s /home/ele/dotfiles/shellscripts/* /usr/local/bin/
