707229cf6a99083490656f31c1330266fac99c49
derele <emanuelheitlinger@gmail.com>
2011-06-30 17:27:47 +0200

    oberon second working form everywhere going over beagle

--- shellscripts/oberon-ssh.sh
+++ shellscripts/oberon-ssh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ssh -Y  -c arcfour,blowfish-cbc -C emanuel@oberon.bio.ed.ac.uk -t 'screen -x';
+ssh -t -Y  -c arcfour,blowfish-cbc -C beagle 'ssh -Y  -c arcfour,blowfish-cbc -C emanuel@oberon.bio.ed.ac.uk -t 'screen -x'';
 if [ $? -ne 0 ] ;
 then
     echo 'No route to host oberon.bio.ed.ac.uk' ;
