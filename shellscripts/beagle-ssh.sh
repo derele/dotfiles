0b7257c13085b889f42b4d6ac88856e23ba0899f
derele <emanuelheitlinger@gmail.com>
2012-03-08 15:10:48 +0100

    aspell and beagle

--- shellscripts/beagle-ssh.sh
+++ shellscripts/beagle-ssh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ssh -Y  -c arcfour,blowfish-cbc -C 129.13.174.229 -t 'screen -x';
+ssh -Y  -c arcfour,blowfish-cbc -C beagle -t 'screen -x';
 if [ $? -ne 0 ] ;
 then
     echo 'No route to host beagle' ;
