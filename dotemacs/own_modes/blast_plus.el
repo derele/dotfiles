d4e55328ee3f34cc22b8cde9e0098692442cda05
Emanuel Heitlinger <emanuelheitlinger@gmail.com>
2013-12-19 18:20:59 +0100

    Finally blasting under point in emacs

---
+++ dotemacs/own_modes/blast_plus.el
@@ -0,0 +1,45 @@
+(require 'thingatpt)
+
+
+;; probably better way to do it
+;; (defun run-blastn ()
+;;   "Run blastn for the sequence under point."
+;;   (interactive)
+;;   (setq cmd (concat "blastn -query <(echo -e \""
+;;                     (thing-at-point 'word)
+;;                     "\") -db /data/db/blastdb/nt/nt "))
+;;   (message (concat "blastn" arg))
+;;   (start-process "*run-blastn*" "*run-blastn*" "blastn" arg)
+;;   (pop-to-buffer "*run-blastn*"))
+
+(defun run-blastn ()
+  "Run blastn for the sequence under point."
+  (interactive)
+  (setq cmd (concat "blastn -query <(echo -e \""
+                    (thing-at-point 'word)
+                    "\") -db /data/db/blastdb/nt/nt &"))
+  (message cmd)
+  (shell-command cmd))
+(global-set-key  "\C-cn" 'run-blastn)
+
+
+(defun run-blastx ()
+  "Run blastx for the sequence under point."
+  (interactive)
+  (setq cmd (concat "blastx -query <(echo -e \""
+                    (thing-at-point 'word)
+                    "\") -db /data/db/blastdb/nr/nr &"))
+  (message cmd)
+  (shell-command cmd))
+(global-set-key  "\C-cx" 'run-blastx)
+
+
+(defun run-blastp ()
+  "Run blastp for the sequence under point."
+  (interactive)
+  (setq cmd (concat "blastp -query <(echo -e \""
+                    (thing-at-point 'word)
+                    "\") -db /data/db/blastdb/nr/nr &"))
+  (message cmd)
+  (shell-command cmd))
+(global-set-key  "\C-cp" 'run-blastp)
