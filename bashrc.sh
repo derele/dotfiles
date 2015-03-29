06394bced5c8223ec87f91335c9bacdd73762a5d
Emanuel Heitlinger <ele@Heitlingernb-1.fritz.box>
2015-03-29 23:02:25 +0200

    added carbon x1 hostname to bashrc sourced for laptop clients

--- bashrc.sh
+++ bashrc.sh
@@ -43,7 +43,7 @@ export PATH=$PATH:/home/ele/bin:/home/ele/tools/R-devel/bin:/home/ele/bin/x86_64
 
 source /home/ele/dotfiles/shell_functions.sh
 
-if [ $HOSTNAME == thinkpad ] || [ $HOSTNAME == ele-laptop ]
+if [ $HOSTNAME == thinkpad ] || [ $HOSTNAME == ele-laptop ]  || [ $HOSTNAME == Heitlingernb-1 ]
 then
     source /home/ele/dotfiles/lap_bash.sh
 else
